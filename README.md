0ca2fab70f8491abc7082fc08bf06382c19ab2dd
wangnan <121971904@qq.com>
2019-04-29 21:04:00 +0800

    .gitignore文件错误的模糊匹配

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 2. 执行 sh build.sh 或 build.cmd ；
 3. 使用以下命令创建项目（注意 -DarchetypeArtifactId 的值加后缀 -archetype ，否者类型不匹配；可选值：maven-archetype-simple-archetype、maven-archetype-webapp-archetype、maven-archetype-webapp-with-multi-modules-archetype）： 
     > 1. cd YOUR_WORKSPACE
-    > 2. mvn archetype:generate -DarchetypeGroupId=com.caiya -DarchetypeArtifactId=maven-archetype-simple-archetype -DarchetypeVersion=1.0.0-SNAPSHOT -DgroupId=com.aaa.bbb -DartifactId=xxx -Dversion=1.0.0-SNAPSHOT -DarchetypeCatalog=local -X
+    > 2. mvn archetype:generate -DarchetypeGroupId=com.caiya -DarchetypeArtifactId=maven-archetype-simple-archetype -DarchetypeVersion=1.0.0-SNAPSHOT -DgroupId=YOUR_PROJECT_GROUP_ID -DartifactId=YOUR_PROJECT_ARTIFACT_ID -Dversion=1.0.0-SNAPSHOT -DarchetypeCatalog=local -X
 
 #### 参考
 > * https://maven.apache.org/archetype/maven-archetype-plugin/examples/create-multi-module-project.html
---
+++ maven-archetype-webapp-with-multi-modules/module-web/src/main/java/com/caiya/web/Application.java
@@ -0,0 +1,30 @@
+package com.caiya.web;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 应用启动入口.
+ *
+ * @author wangnan
+ * @since 1.0
+ */
+@SpringBootApplication(/*scanBasePackages = "com.project"*/exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+////@EnableEurekaClient
+//@EnableFeignClients(basePackages = "com.project")
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).run(args);
+    }
+
+}
